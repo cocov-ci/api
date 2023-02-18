cb782dd4f2111229e0badb452199256508f23927
Victor Gama <hey@vito.io>
2023-02-18 20:20:54 -0300

    fix(migration): Ensure the correct method is used to add columns

--- db/migrate/20230218202255_add_timestamps_to_check_sets.rb
+++ db/migrate/20230218202255_add_timestamps_to_check_sets.rb
@@ -3,8 +3,8 @@
 class AddTimestampsToCheckSets < ActiveRecord::Migration[7.0]
   def change
     change_table :check_sets, bulk: true do |t|
-      t.add_column :finished_at, :timestamp
-      t.add_column :started_at, :timestamp
+      t.column :finished_at, :timestamp
+      t.column :started_at, :timestamp
     end
   end
 end
