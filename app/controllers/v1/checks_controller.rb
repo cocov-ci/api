f991ade38e43cfee129f2a624d992040da1c6c14
Victor Gama <hey@vito.io>
2023-03-09 20:55:20 -0300

    fix(Checks): Allow non-service-tokens to call re_run and cancel

--- app/controllers/v1/checks_controller.rb
+++ app/controllers/v1/checks_controller.rb
@@ -3,7 +3,7 @@
 module V1
   class ChecksController < V1Controller
     before_action :ensure_authentication
-    before_action :ensure_service_token, only: %i[patch wrap_job_up cancel re_run notify_processing]
+    before_action :ensure_service_token, only: %i[patch wrap_job_up notify_processing]
 
     def index
       repo = Repository.with_context(auth_context).find_by!(name: params[:repo_name])
